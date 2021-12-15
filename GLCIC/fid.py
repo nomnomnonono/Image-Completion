be7f9e4a858186e1cf9861341728b951aa11fd87
nomnomnonono <murumuru1204@gmail.com>
2021-12-15 13:52:01 +0900

    add module of calculate FID

---
+++ GLCIC/fid.py
@@ -0,0 +1,44 @@
+import numpy as np
+from scipy import linalg
+import torch
+from torchvision import models
+from PIL import Image
+from torch.nn import functional as F
+from torchvision import transforms
+
+
+
+def fid_torch(x_true, x_false, device, mode='bicubic'):
+    # load Inception-v3
+    model = models.inception_v3(pretrained=True).to(device)
+    model.dropout = torch.nn.Identity()
+    model.fc = torch.nn.Identity()
+    model = model.eval()
+    # resize
+    x_true = F.resize(x_true, 299, mode=mode)
+    x_false = F.resize(x_false, 299, mode=mode)
+    # get output
+    out_true = model(x_true).detach().to('cpu').numpy()
+    out_false = model(x_false).detach().to('cpu').numpy()
+    # calculate mu, sigma
+    mu_true = np.mean(out_true, 0)
+    mu_false = np.mean(out_false, 0)
+    sigma_true = np.cov(out_true, rowvar=False)
+    sigma_false = np.cov(out_false, rowvar=False)
+    # calculate fid
+    mu_diff = mu_true - mu_false
+    cov = linalg.sqrtm(sigma_true.dot(sigma_false)).real
+    fid = mu_diff.dot(mu_diff) - np.trace(sigma_true + sigma_false - 2*cov)
+    return fid
+
+
+def fid_td():
+    return
+
+
+def fid_pil():
+    return
+
+
+def fid_cv():
+    return
